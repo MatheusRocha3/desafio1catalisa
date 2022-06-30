19b3bbf442b1fdd08dd29c9f997f86a27c27eeb4
MatheusRocha3 <matheuusrocha012@gmail.com>
2022-06-30 15:37:22 -0300

    Terceira versão do jogo Acerte o Numero Aleatorio

--- src/AcerteONumero.java
+++ src/AcerteONumero.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
     public class AcerteONumero {
@@ -10,20 +9,19 @@ import java.util.Scanner;
             System.out.println("Bem vindo ao nosso incrível jogo! Aqui, iremos sortear um número e você deve acertar!");
             System.out.println("Mas antes, vou te explicar como funciona o jogo.  Iremos sortear um número e você deve acertar o número que sorteamos. \n " +
                     "Caso acerte, você ganha 10 pontos e continuamos a jogar. \nCaso o número sorteado seja 30 e você escolha o número 29 ou 31, você ganha 5 pontos e ganha mais uma chance de acertar o número. \n Caso erre e não tenha pontos acumulados, game over. \n");
-            System.out.print("Nesse jogo temos 1 nível: Fácil. O nível fácil vai até o número 20. Boa sorte!");
-            System.out.print(" \n │------------------------------------------------------│\n");
-            System.out.print("Opção 1 - Nível de dificuldade fácil \n");
-            int opcao = (input.nextInt());
-
+            System.out.println("Nesse jogo temos 1 nível: Fácil. O nível fácil vai até o número 20. Boa sorte!");
+            System.out.println("Para começar o jogo digite 1. Qualquer outro número ou tecla o jogo não irá carregar!");
+            int nivel = (input.nextInt());
                 while (true)
-                    if (opcao == 1) {
-                        System.out.println("Você escolheu a dificuldade fácil!");
-                        System.out.println("Escolha um número de 0 até 10");
+                    if (nivel == 1) {
+                        System.out.println("Você está jogando na difuldade fácil!");
+                        System.out.println("Escolha um número de 0 até 20");
                         int escolhaNumeroUsuario = (input.nextInt());
-                        int numeroAleatorio = random.nextInt(11);
+                        int numeroAleatorio = random.nextInt(6);
+                        System.out.println(numeroAleatorio);
                         int numeroMaximo = numeroAleatorio + 1;
                         int numeroMinimo = numeroAleatorio - 1;
-                        if (Objects.equals(escolhaNumeroUsuario, numeroAleatorio)) {
+                        if (escolhaNumeroUsuario == numeroAleatorio) {
                             pontos = pontos + 10;
                             System.out.println("Parabéns, você acertou!");
                             System.out.println("Seus pontos atuais são: " + pontos);
@@ -33,15 +31,12 @@ import java.util.Scanner;
                             System.out.println("Seus pontos atuais são: " + pontos);
                             System.out.println("Deseja tentar novamente? Digite um número de 1 até 20");
                             segundaChance = input.nextInt();
-                        }
-                        if (segundaChance == numeroAleatorio) {
+                        } if (segundaChance == numeroAleatorio) {
                             pontos = pontos + 5;
                             System.out.println("Você acertou!");
                             System.out.println("Sua pontuação atual é: " + pontos);
-                        } else {
-                            System.out.println("Você perdeu. Sua pontuação final é: " + pontos);
-                        }
-                        if (escolhaNumeroUsuario != numeroMaximo && segundaChance != numeroAleatorio) {
+                        }if (escolhaNumeroUsuario != numeroAleatorio && segundaChance != numeroAleatorio) {
+                            System.out.println("Sua pontuação final é: " + pontos);
                             break;
                         }
                     }
