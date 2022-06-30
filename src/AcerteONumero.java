070c1acf288f8ef6076c9cb6e7529350ad425e35
MatheusRocha3 <matheuusrocha012@gmail.com>
2022-06-30 14:57:05 -0300

    Segunda versão do jogo Acerte o Numero Aleatorio

--- src/AcerteONumero.java
+++ src/AcerteONumero.java
@@ -10,17 +10,10 @@ import java.util.Scanner;
             System.out.println("Bem vindo ao nosso incrível jogo! Aqui, iremos sortear um número e você deve acertar!");
             System.out.println("Mas antes, vou te explicar como funciona o jogo.  Iremos sortear um número e você deve acertar o número que sorteamos. \n " +
                     "Caso acerte, você ganha 10 pontos e continuamos a jogar. \nCaso o número sorteado seja 30 e você escolha o número 29 ou 31, você ganha 5 pontos e ganha mais uma chance de acertar o número. \n Caso erre e não tenha pontos acumulados, game over. \n");
-            System.out.print("Nesse jogo temos 3 níveis: Fácil, médio, díficil e impossível. \nFácil vai até o numero 10, médio até o número 30, díficil até o 60 e impossível até o 100. Boa sorte!");
+            System.out.print("Nesse jogo temos 1 nível: Fácil. O nível fácil vai até o número 20. Boa sorte!");
             System.out.print(" \n │------------------------------------------------------│\n");
             System.out.print("Opção 1 - Nível de dificuldade fácil \n");
-            System.out.print("Opção 2 - Nível de dificuldade médio \n");
-            System.out.print("Opção 3 - Nível de dificuldade difícil \n");
-            System.out.print("Opção 4 - Nível de dificuldade impossível \n");
-            System.out.print("\n│-----------------------------------------------------------│");
-            System.out.println(" Escolha a opção \n");
             int opcao = (input.nextInt());
-
-            {
 
                 while (true)
                     if (opcao == 1) {
@@ -54,5 +47,5 @@ import java.util.Scanner;
                     }
             }
         }
-    }
+
 
