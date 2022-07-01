6a9d897d9c6297206b022c7df501742d61be926d
MatheusRocha3 <matheuusrocha012@gmail.com>
2022-06-30 21:55:15 -0300

    Versão final do jogo Acerte o Numero

--- src/AcerteONumero.java
+++ src/AcerteONumero.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
             System.out.println("Bem vindo ao nosso incrível jogo! Aqui, iremos sortear um número e você deve acertar!");
             System.out.println("Mas antes, vou te explicar como funciona o jogo.  Iremos sortear um número e você deve acertar o número que sorteamos. \n " + "Caso acerte, você ganha 10 pontos e continuamos a jogar. \nCaso o número sorteado seja 30 e você escolha o número 29 ou 31, você ganha 5 pontos e ganha mais uma chance de acertar o número. \n Caso erre e não tenha pontos acumulados, game over. \n");
             System.out.println("Por enquanto nosso jogo só tem uma dificuldade. Esta dificuldade vai do 0 até o 20, boa sorte!r!");
+            System.out.println("Digite 1 para iniciar. Qualquer outra tecla encerrará o jogo.");
             int nivel = (input.nextInt());
             while (true)
                 if (nivel == 1) {
@@ -19,7 +20,14 @@ import java.util.Scanner;
                     int numeroMaximo = numeroAleatorio + 1;
                     int numeroMinimo = numeroAleatorio - 1;
                     {
-                        }
+                        if (escolhaNumeroUsuario > 20);
+                        System.out.println("Número inválido.");
+                        System.out.println("Digite um número de 0 a 20");
+                        escolhaNumeroUsuario = input.nextInt();
+                        }  if (escolhaNumeroUsuario < 1);
+                    System.out.println("Número inválido");
+                    System.out.println("Digite um número de 0 a 20");
+                    escolhaNumeroUsuario = input.nextInt();
                         if (escolhaNumeroUsuario == numeroAleatorio) {
                             pontos = pontos + 10;
                             System.out.println("Parabéns, você acertou!");
